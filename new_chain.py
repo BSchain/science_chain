f1c00adfc4648a833a877b447e6c0ef979796b32
zpfbuaa <zpflyfe@163.com>
2018-01-05 15:23:26 +0800

    codes in new_chain

--- new_chain.py
+++ new_chain.py
@@ -8,41 +8,33 @@
 from time import time
 import json
 import hashlib as hasher
+import os
+from urllib.parse import urlparse
+from uuid import uuid4
+import requests
 
 class Blockchain():
     def __init__(self):
-        self.chain_length = self.get_chain_length()
-        self.last_block = {}
+        self.file_init()
+        self.chain = []
         self.current_transactions = []
-        self.current_diff = self.get_diff()
+        self.last_block = []
+        self.chain_length = 0
+        self.last_diff = 5
+        self.block_root = "blocks/"
+        self.transactions_file = "current_transactions"
+        self.nodes = set()
 
-    def get_chain_length(self): # next_block_index = chain_length + 1 = len(dir) + 1
-        return 1
-        pass
-
-    def get_last_block(self): # read file and get the last block
-        pass
-
-    def get_total_block(self):
-        pass
-
-    def get_block_by_height(self): # read file and get one block
-        pass
-
-    # we can change diff or create diff rules.
-    def get_diff(self): # generate the difficult to mine new block
-        pass
-
-    def save_block(self): # when mine one block then update block and store new block to file
-        pass
-
-    def get_current_transactions(self): # generate transaction from file
-        pass
-
-    @staticmethod
-    def hash(block):
-        block_str = json.dumps(block, sort_keys=True).encode()
-        return hasher.sha256(block_str).hexdigest()
+    def genersis_block(self,proof=100,previous_hash='1'):
+        first_block = {
+            'index': 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash,
+        }
+        self.save_block(block=first_block)
+        return first_block
 
     def generate_new_block(self, proof, previous_hash=None): # generate new block from transaction
         block = {
@@ -50,18 +42,120 @@ class Blockchain():
             'timestamp': time(),
             'transactions': self.current_transactions,
             'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.last_block),
+            'previous_hash': previous_hash or self.hash(self.get_last_block()),
         }
         # Reset the current list of transactions
-        self.current_transactions = []
+        self.reset_current_transactions()
+        self.save_block(block)
         self.last_block = block
-        self.save_block()
         self.chain_length += 1
-        pass
+
+    def register_node(self,address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self,chain): # to valid the whole chain
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print('\n-------------\n')
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            if not self.valid_proof(last_block['proof'],block['proof']):
+                return False
+            last_block = block
+            current_index +=1
+        return True
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = self.get_chain_length()
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+    def file_init(self):
+        if os.path.exists(self.block_root) == False:
+            os.mkdir(self.block_root)
+        if os.path.exists(self.transactions_file) == False:
+            file = open(self.transactions_file, 'r')
+            file.close()
+
+    def get_chain_length(self): # next_block_index = chain_length + 1 = len(dir) + 1
+        block_list = os.listdir(self.block_root)
+        return len(block_list)
+
+    def get_last_block(self): # read file and get the last block
+        with open(self.block_root+str(self.chain_length)) as file_json:
+            block = json.load(file_json)
+        return block
+
+    def get_whole_chain(self):
+        whole_chain = []
+        for height in range(self.chain_length):
+            with open(self.block_root+str(height+1),'r') as f:
+                block = json.load(f)
+            whole_chain.append(block)
+        return whole_chain
+
+    def get_block_by_height(self,height): # read file and get one block
+        block_height = {}
+        if os._exists(self.block_root+ str(block_height)) == False or (height > self.chain_length):
+            return block_height
+        #assert height <= self.chain_length, ("height:",height," chain_length:",self.chain_length)
+        with open(self.block_root+height,'r') as f:
+            block_height = json.load(f)
+        return block_height
+
+
+    def get_current_diff(self): # generate the difficult to mine new block
+        """
+        # we can change diff or create diff rules.
+        :return:
+        """
+        return self.last_diff
+
+    def save_block(self,block): # when mine one block then update block and store new block to file
+        index = block['index']
+        with open(self.block_root+index, 'w') as json_file:
+            json_file.write(json.dumps(block))
+
+    def get_current_transactions(self): # generate transaction from file
+        with open(self.transactions_file,'r') as f:
+            current_transactions = json.load(f)
+        return current_transactions
+
+    def reset_current_transactions(self): # empty the old transactions
+        with open(self.transactions_file) as file:
+            file.truncate()
+        self.current_transactions = []
+
+    def store_one_transactions(self,one_transaction):
+        with open(self.transactions_file,'a+') as json_file:
+            json_file.write(json.dumps(one_transaction))
+
+    def hash(self,block): # get block hash
+        block_str = json.dumps(block, sort_keys=True).encode()
+        return hasher.sha256(block_str).hexdigest()
 
     def proof_of_work(self,last_proof): # proof of work
         proof = 0
-        while (self.valid_proof(last_proof, proof,self.current_diff) is False):
+        while (self.valid_proof(last_proof, proof,self.get_current_diff()) is False):
             proof = proof + 1
         return proof
 
@@ -72,7 +166,6 @@ class Blockchain():
         diff_str = '0' * diff
         return mine_hash[:diff] == diff_str
 
-        pass
 
 
 
